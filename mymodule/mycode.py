374b2be2bd9192b7d785e364cb4a262d6ae2b9d8
sp suryawanshi <59173100+MrSpSuryawanshi@users.noreply.github.com>
2023-10-12 13:12:09 +0530

    Update mycode.py

--- mymodule/mycode.py
+++ mymodule/mycode.py
@@ -1,22 +1,6 @@
-   from pylint.interfaces import IRawChecker
-   from pylint.checkers import BaseChecker
+   # mypylintproject/mymodule/mycode.py
 
-   class HelloChecker(BaseChecker):
-       __implements__ = IRawChecker
+   def my_function():
+       print("Hello, world!")
 
-       name = 'hello-checker'
-       msgs = {
-           'W0001': (
-               'Hello detected in the code',
-               'warning-hello',
-               'Used when "hello" is found in the code'
-           )
-       }
-
-       def process_module(self, node):
-           for line_num, line in enumerate(node.stream(), start=1):
-               if 'hello' in line:
-                   self.add_message('warning-hello', line=line_num)
-
-   def register(linter):
-       linter.register_checker(HelloChecker(linter))
+   my_function()
